cb75f4935d17113366d3d3a52fc651fb8396f1b4
WushanCloud <55572755+WushanCloud@users.noreply.github.com>
2020-04-12 22:31:02 +0800

    Create 合并两个排序的链表.cpp

---
+++ 合并两个排序的链表.cpp
@@ -0,0 +1,48 @@
+题目描述
+输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        ListNode* newhead = new ListNode(0);
+        ListNode* cur     = newhead;
+        //开始替换
+        while(pHead1 && pHead2)
+        {
+            if (pHead1->val < pHead2->val)
+            {
+                cur->next = pHead1;
+                pHead1 = pHead1->next;
+                cur = cur->next;
+            }
+            else
+            {
+                cur->next = pHead2;
+                pHead2 = pHead2->next;
+                cur = cur->next;
+            }
+        }
+        if(pHead1)
+        {
+            cur->next = pHead1;
+        }
+        if(pHead2)
+        {
+            cur->next = pHead2;
+        }
+        return newhead->next;
+    }
+};
+
+
+
+
